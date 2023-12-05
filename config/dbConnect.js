1c1cb2b5abe926a1bdbe1ddf91f30ceda63be18d
OdilovMM <dssrinman@gmail.com>
2023-12-06 03:48:39 +0900

    add: adding usermodel schema

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = () => {
   try {
-    const con = mongoose.connect("mongodb://localhost:27017/shopme");
+    const con = mongoose.connect(process.env.MONGODB_URL);
     console.log("MongoDB connected successfully");
   } catch (err) {
     throw new Error(err);
