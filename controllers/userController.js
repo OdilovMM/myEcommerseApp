55c05007608a685f3a7e42b0573767d7ba11af86
OdilovMM <dssrinman@gmail.com>
2023-12-06 04:54:40 +0900

    feat: Adding errorHandler middleware, and registering to index.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,16 +3,19 @@ const User = require("../models/userModel");
 const createUser = async (req, res) => {
   // 1. check if user already exist
   const email = req.body.email;
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email: email });
   if (!user) {
     // if no user exist, create a new user
-    const newUser = User.create(req.body);
+    const newUser = await User.create(req.body);
     res.status(201).json({
-      message: "success",
-      user: newUser,
+      status: "success",
+      data: {
+        data: newUser,
+      },
     });
   } else {
     // user already exist
+
     res.status(403).json({
       message: "User already exist",
       success: false,
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+var bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const app = express();
+const morgan = require("morgan");
 const dotenv = require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 dbConnect();
 
+app.use(morgan("dev"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // registering router handlers
 app.use("/api/user", authRoutes);
+
+
+
+
+
+// error handler middleware
+app.use(notFound)
+app.use(errorHandler)
+
+
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
