4314fd082b378d60786d9efedb3000c4e0953521
OdilovMM <dssrinman@gmail.com>
2023-12-06 03:57:24 +0900

    fix: fixing userModel.js and modifying, adding authRoutes and registering router to index.js

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+const authRoutes = require("./routes/authRoutes");
+
 dbConnect();
 
 app.use("/", (req, res) => {
   res.send("Hello from the server side!!!");
 });
 
+// registering router handlers
+app.use("/api/user", authRoutes);
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
